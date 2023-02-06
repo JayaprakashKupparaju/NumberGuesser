31b24ac0b0c8b277ff54fc3e2d8f6b77171f3950
Jayaprakash <jkupparaju@yahoo.com>
2023-02-05 21:27:34 -0600

    Finished number guesser game

---
+++ NumberGuesser/NumberGuesser/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuesser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int guess;
+            bool correct = false;
+            Random random = new Random();
+            int num = random.Next(0,100);
+           
+            do
+            {
+                
+                Console.WriteLine("Guess my number which is between 0 and 100");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess == num)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Correct");
+                    correct = true;
+                    Console.ResetColor();
+                }
+                else if (guess < num)
+                {
+                    Console.WriteLine("Number is greater than the guessed number");
+                }
+                else if (guess > num) {
+                    Console.WriteLine("Number is lesser than the guessed number");
+                }
+
+            } while (!correct);
+
+            Console.ReadLine();
+        }
+    }
+}
